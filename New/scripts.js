94d7b37e191fe25308941487f44f9c74fa01c3aa
Decska <66358871+Decska@users.noreply.github.com>
2020-06-15 21:43:47 +0200

    Java practice

---
+++ New/scripts.js
@@ -0,0 +1,18 @@
+//$(selector).action()
+//$("div").text("Kati");
+$("div").css("background", "pink")
+
+//to remove things: toggle=change "first" with "hidden"
+$(".first").toggleClass("hidden")
+$("p").html("<strong>Hurrá! Fog ez menni!</strong>")
+
+//append: puts it to the end, remove: removes
+$("p").append(" I'm great!")
+
+$("button").click((event) => {
+    $(".first").toggleClass("hidden")
+})
+$("button").click((event) => {
+    $(".first").toggleClass("third")
+})
+
